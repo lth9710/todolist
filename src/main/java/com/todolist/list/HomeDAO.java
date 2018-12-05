32e8bbb68853b77f219d564225fa303b3939d5c4
lth9710 <lth9710@naver.com>
2018-12-05 23:08:04 +0900

    encoding with second commit

---
+++ src/main/java/com/todolist/list/HomeDAO.java
@@ -0,0 +1,28 @@
+package com.todolist.list;
+
+import java.util.List;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HomeDAO {
+
+	@Autowired
+	private SqlSessionTemplate sessionTemplate;
+
+	public void setSessionTemplate(SqlSessionTemplate sessionTemplate) {
+		this.sessionTemplate = sessionTemplate;
+	}
+	
+	public List<HomeDTO> getList() {
+
+
+		List<HomeDTO> lists = sessionTemplate.selectList("listMapper.getLists");
+
+		return lists;
+
+	}
+	
+	
+}
---
+++ src/main/java/com/todolist/list/HomeDTO.java
@@ -0,0 +1,62 @@
+package com.todolist.list;
+
+public class HomeDTO {
+
+	
+	private int listNum;
+	private String title;
+	private String content;
+	private String createDate;
+	private String closingDate;
+	private String status;
+	private int priorityNum;
+	
+	
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getClosingDate() {
+		return closingDate;
+	}
+	public void setClosingDate(String closingDate) {
+		this.closingDate = closingDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getPriorityNum() {
+		return priorityNum;
+	}
+	public void setPriorityNum(int priorityNum) {
+		this.priorityNum = priorityNum;
+	}
+	
+	
+	
+	
+	
+}
